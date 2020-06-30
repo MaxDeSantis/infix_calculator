e703ae4607f3dadd879384de376c5c8e265dd640
Max DeSantis <desantis.max.a@gmail.com>
2020-06-29 21:33:28 -0500

    initial setup

---
+++ infixcalculator.cpp
@@ -0,0 +1,28 @@
+/*
+infixcalculator.cpp
+
+Course:                 CS 2433, C/C++ Programming
+Semester:               Summer 2020
+Assignment:             Honors Contract
+Instructor:             Richard Churchill
+Due Date/Time:          Julyy 31, 2020
+Submitted:          	
+Written by:             Max DeSantis
+
+Build Instructions:
+
+
+Usage:
+
+*/
+
+
+/*Notes before I start:
+what's the best way to go about this?
+
+
+*/
+
+int main(int argc, char* argv[]) {
+
+}
